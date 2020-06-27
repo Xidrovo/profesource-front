36a9767da49471f997fdc6179ca442bcc0c4b82d
fponce06 <fponce@espol.edu.ec>
2020-06-26 21:31:20 -0500

    fix of details

--- src/components/PopularPost.js
+++ src/components/PopularPost.js
@@ -3,14 +3,13 @@ import Button from '@components/Button'
 
 const PopularPost = () => {
         return (
-            <div className=" md:ml-24 border-solid border-2 border-gray-10 text-base h-auto ml-12 mb-8 w-1/2">
-                <div >
-                <img class="float-right ml-4 my-2 h-32" src="https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80https://imgur.com/a/0Jlvibw"/>
+            <div className="rounded-lg md:ml-24 border-solid border-2 border-gray-101 text-base h-auto ml-12 mb-8 w-3/4">
+                <div>
+                    <img class="float-right ml-4 my-2 h-32 m-5" src="https://images.unsplash.com/photo-1459262838948-3e2de6c1ec80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80https://imgur.com/a/0Jlvibw"/>
                     <h2 class="p-3 text-justify">Mas popular</h2>
-                    <p class="p-3 text-justify">Post mas popular jsdkjgfjg lfkkhfohlllkylkflyfmkfj fkljhfkjh fkgjkfjg sd gh .</p>
+                    <p class="p-3 text-justify">Post mas popular</p>
                     <Button />
                 </div>
-
             </div>
         );
     }
