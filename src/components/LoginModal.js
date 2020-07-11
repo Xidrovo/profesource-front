3d459a2a3cb1585c1f72dfd32f3313377830b631
CarlosXavier <cxcarvaj@espol.edu.ec>
2020-07-10 21:58:00 -0500

    Add Login Modal

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Link } from 'gatsby'
+import UseOnClickOutside from '../hooks/useOnClickOutside';
 
 
 
 const LoginModal = (props) => {
+  const refLoginModal = useRef();
+  UseOnClickOutside(refLoginModal, () => props.setShowModal(false));
 
   return (
     <div className="bg-opacity-75 bg-black justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none transition ease-out duration-700"
       >
-      <div class="rounded-lg bg-gray-100 w-1/2 min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div class="rounded-lg bg-gray-100 w-1/2 min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8"
+      ref ={refLoginModal}>
         <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10 sm:mx-auto sm:w-full sm:max-w-md">
           {/* logo de la pagina */}
           {/* <img class="mx-auto h-12 w-auto" src="../images/gatsby-icon.png" alt="Workflow" /> */}
