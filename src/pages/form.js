bf546500ef28810b8843d75537667b5fb4e17622
fponce06 <fponce@espol.edu.ec>
2020-07-11 15:22:22 -0500

    Styles of form page

--- src/pages/form.js
+++ src/pages/form.js
@@ -4,22 +4,23 @@ import { Link } from 'gatsby'
 import SEO from '../components/seo'
 import Layout from '@components/Layout'
 
-const FormPage = () => {
+const FormPage = (props) => {
+  var LOCATION_KEY = 'AIzaSyAp8PdAo33pwVlNZKxlmbcHUOBeTfghXW4'
   return (
-    <Layout title="Form">
-      <SEO title="Contactanos" />
+    <Layout title="Contáctanos">
+      <SEO title="Contáctanos" />
       <form>
-        <div>
+        <div class="px-6">
           <div class="mt-8 border-t border-gray-200 pt-8">
-            <div>
-              <h3 class="text-lg leading-6 font-medium text-gray-900">
-                Datos Personal
+            <div class="rounded-t-lg border-2 border-opacity-25 border-gray-600 p-3 bg-gray-800">
+              <h3 class="text-lg leading-6 font-medium text-white">
+                Datos Personales
               </h3>
               <p class="mt-1 text-sm leading-5 text-gray-500">
-                Use a permanent address where you can receive mail.
+                Usa un correo fijo donde puedas ser contactado
               </p>
             </div>
-            <div class="mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
+            <div class="rounded-b-lg border-2 border-opacity-25 border-gray-600 p-3 mb-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
               <div class="sm:col-span-3">
                 <label
                   for="first_name"
@@ -30,7 +31,7 @@ const FormPage = () => {
                 <div class="mt-1 rounded-md shadow-sm">
                   <input
                     id="first_name"
-                    class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                    class="form-input shadow appearance-none border rounded w-full text-gray-700 leading-tight focus:shadow-outline transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                   />
                 </div>
               </div>
@@ -45,7 +46,22 @@ const FormPage = () => {
                 <div class="mt-1 rounded-md shadow-sm">
                   <input
                     id="last_name"
-                    class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                    class="form-input shadow appearance-none border rounded w-full text-gray-700 leading-tight  focus:shadow-outline transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
+                </div>
+              </div>
+
+              <div class="sm:col-span-4">
+                <label
+                  for="cell-phone"
+                  class="block text-sm font-medium leading-5 text-gray-700"
+                >
+                  Celular
+                </label>
+                <div class="mt-1 rounded-md shadow-sm">
+                  <input
+                    id="cell-phone"
+                    class="form-input shadow appearance-none border rounded w-full text-gray-700 leading-tight  focus:shadow-outline transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                   />
                 </div>
               </div>
@@ -61,7 +77,7 @@ const FormPage = () => {
                   <input
                     id="email"
                     type="email"
-                    class="form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                    class="form-input shadow appearance-none border rounded w-full text-gray-700 leading-tight  focus:shadow-outline transition duration-150 ease-in-out sm:text-sm sm:leading-5"
                   />
                 </div>
               </div>
@@ -74,39 +90,75 @@ const FormPage = () => {
                   Detalle
                 </label>
                 <div class="mt-1 rounded-md shadow-sm">
-                  <textarea id="detail" rows="20" cols="50" />
+                  <textarea
+                    id="detail"
+                    rows="10"
+                    cols="50"
+                    class="form-input shadow appearance-none border rounded w-full text-gray-700 leading-tight  focus:shadow-outline transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
                 </div>
               </div>
-
-              <div class="sm:col-span-3">
-                <label
-                  for="zona"
-                  class="block text-sm font-medium leading-5 text-gray-700"
-                >
-                  Zona
-                </label>
-                <div class="mt-1 rounded-md shadow-sm">
-                  <select
-                    id="zona"
-                    class="form-select block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+            </div>
+            <div>
+              <div class="rounded-t-lg border-2 border-opacity-25 border-gray-600 p-3 bg-gray-800">
+                <h3 class="text-lg leading-6 font-medium text-white">
+                  Domicilio
+                </h3>
+              </div>
+              <div class="rounded-b-lg border-2 border-opacity-25 border-gray-600 p-3 mb-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
+                <div class="sm:col-span-3">
+                  <label
+                    for="zona"
+                    class="block text-sm font-medium leading-5 text-gray-700"
                   >
-                    <option>Villaclub</option>
-                    <option>Guasmo</option>
-                    <option>Florida</option>
-                    <option>Sauces</option>
-                    <option>Mapasingue</option>
-                    <option>Sopeña</option>
-                  </select>
+                    Zona
+                  </label>
+                  <div class="mt-1 rounded-md shadow-sm">
+                    <select
+                      id="zona"
+                      class="form-input shadow appearance-none border rounded w-full text-gray-700 leading-tight  focus:shadow-outline transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                    >
+                      <option>Villaclub</option>
+                      <option>Guasmo</option>
+                      <option>Florida</option>
+                      <option>Sauces</option>
+                      <option>Mapasingue</option>
+                      <option>Sopeña</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div class="sm:col-span-4">
+                  <label
+                    for="ubicacion"
+                    class="block text-sm font-medium leading-5 text-gray-700"
+                  >
+                    Ubicacion
+                  </label>
+                  <div class="mt-1 rounded-md shadow-sm">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={`https://www.google.com.ec/maps/place/Escuela+Superior+Polit%C3%A9cnica+del+Litoral+(ESPOL)/@-2.1481404,-79.9666772,17z/data=!3m1!4b1!4m5!3m4!1s0x902d72f925e5bfdb:0x327cdb9f7f4ba3b2!8m2!3d-2.1481458!4d-79.9644885`}
+                      className="mt-1 text-left"
+                    >
+                      <img
+                        id="mapa"
+                        alt="Ubicación"
+                        crossOrigin="anonymous"
+                        className="w-auto"
+                        src={`https://www.google.com.ec/maps/place/Escuela+Superior+Polit%C3%A9cnica+del+Litoral+(ESPOL)/@-2.1481404,-79.9666772,17z/data=!3m1!4b1!4m5!3m4!1s0x902d72f925e5bfdb:0x327cdb9f7f4ba3b2!8m2!3d-2.1481458!4d-79.9644885&key=${LOCATION_KEY}`}
+                      />
+                    </a>
+                  </div>
                 </div>
               </div>
-
-              
             </div>
           </div>
           <div class="mt-8 border-t border-gray-200 pt-8">
             <div>
               <h3 class="text-lg leading-6 font-medium text-gray-900">
-                Notifications
+                Notificaciones
               </h3>
               <p class="mt-1 text-sm leading-5 text-gray-500">
                 We'll always let you know about important changes, but you pick
@@ -129,10 +181,11 @@ const FormPage = () => {
                     </div>
                     <div class="ml-3 text-sm leading-5">
                       <label for="comments" class="font-medium text-gray-700">
-                        Comments
+                        Comentarios
                       </label>
                       <p class="text-gray-500">
-                        Get notified when someones posts a comment on a posting.
+                        Te notificaremos cuando alguien haya respondido a tu
+                        pulicacion
                       </p>
                     </div>
                   </div>
@@ -140,20 +193,18 @@ const FormPage = () => {
                     <div class="relative flex items-start">
                       <div class="flex items-center h-5">
                         <input
-                          id="candidates"
+                          id="new_post"
                           type="checkbox"
                           class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
                         />
                       </div>
                       <div class="ml-3 text-sm leading-5">
-                        <label
-                          for="candidates"
-                          class="font-medium text-gray-700"
-                        >
-                          Candidates
+                        <label for="new_post" class="font-medium text-gray-700">
+                          Nuevas publicaciones
                         </label>
                         <p class="text-gray-500">
-                          Get notified when a candidate applies for a job.
+                          Te notificaremos cuando hayan nuevas publicaciones en
+                          los grupos
                         </p>
                       </div>
                     </div>
@@ -162,18 +213,21 @@ const FormPage = () => {
                     <div class="relative flex items-start">
                       <div class="flex items-center h-5">
                         <input
-                          id="offers"
+                          id="new_groups"
                           type="checkbox"
                           class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
                         />
                       </div>
                       <div class="ml-3 text-sm leading-5">
-                        <label for="offers" class="font-medium text-gray-700">
-                          Offers
+                        <label
+                          for="new_groups"
+                          class="font-medium text-gray-700"
+                        >
+                          Nuevos grupos
                         </label>
                         <p class="text-gray-500">
-                          Get notified when a candidate accepts or rejects an
-                          offer.
+                          Te notificaremos cuando haya una actualizacion de
+                          nuevos grupos
                         </p>
                       </div>
                     </div>
@@ -185,7 +239,7 @@ const FormPage = () => {
                   Push Notifications
                 </legend>
                 <p class="text-sm leading-5 text-gray-500">
-                  These are delivered via SMS to your mobile phone.
+                  Estos son envios via SMS a tu celular
                 </p>
                 <div class="mt-4">
                   <div class="flex items-center">
@@ -197,7 +251,7 @@ const FormPage = () => {
                     />
                     <label for="push_everything" class="ml-3">
                       <span class="block text-sm leading-5 font-medium text-gray-700">
-                        Everything
+                        Todo
                       </span>
                     </label>
                   </div>
@@ -210,7 +264,7 @@ const FormPage = () => {
                     />
                     <label for="push_email" class="ml-3">
                       <span class="block text-sm leading-5 font-medium text-gray-700">
-                        Same as email
+                        Enviar por correo
                       </span>
                     </label>
                   </div>
@@ -223,7 +277,7 @@ const FormPage = () => {
                     />
                     <label for="push_nothing" class="ml-3">
                       <span class="block text-sm leading-5 font-medium text-gray-700">
-                        No push notifications
+                        No enviar notificaciones
                       </span>
                     </label>
                   </div>
@@ -232,7 +286,7 @@ const FormPage = () => {
             </div>
           </div>
         </div>
-        <div class="mt-8 border-t border-gray-200 pt-5">
+        <div class="mt-8 border-t border-gray-200 pt-5 mb-6">
           <div class="flex justify-end">
             <span class="inline-flex rounded-md shadow-sm">
               <button
