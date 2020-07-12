6f342dbae10a7c1421e5f18fcd0e7726c62a5921
kelly <castrofigueroakelly@gmail.com>
2020-07-11 22:03:21 -0500

    changes in size : ProfileCard

--- src/components/profileCard.js
+++ src/components/profileCard.js
@@ -4,24 +4,23 @@ import CalendarIcon from '@Icons/CalendarIcon';
 
 const ProfileCard = (props) =>{
     return(
-        <div className="border border-solid-2 w-64 m-3 rounded rounded-lg">
-            <div className="flex justify-center">
+        <div className=" flex border border-solid-2  m-3 bg-white rounded rounded-lg md:w-64 md:flex-col ">
+            <div className="flex justify-center items-center">
                     <img
-                        className="h-30 w-30 rounded-t-lg"
+                        className="h-30 rounded-md ml-3 my-2 md:rounded-t-lg md:rounded-b-none md:w-64 md:m-0"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
                         />
                 </div>
             <div className="flex-col items-center justify-start space-y-3 m-4">
-                
-                
-                <div className="content-start">
-                    <h5 className="text-xl text-blue-103  font-semibold">{props.name}</h5>
+    
+                <div>
+                    <h5 className=" text-xl text-blue-103  font-semibold">{props.name}</h5>
                     <h5 className="text-lg text-blue-103">{props.username}</h5>
                 </div>
                 <div className=" flex space-x-1">
                     <LocationIcon
-                    className="fill-currenttext-blue-101"
+                    className="fill-current text-blue-101"
                     width="15px"
                     color={'#52658f'}
                     ></LocationIcon>
@@ -30,7 +29,7 @@ const ProfileCard = (props) =>{
                 </div>
                 <div className="flex space-x-1">
                     <CalendarIcon
-                    className="fill-currenttext-blue-101"
+                    className="fill-current text-blue-101"
                     width="15px"
                     color={'#52658f'}
                     >
