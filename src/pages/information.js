14f548feedf2e02554e4dcfb3d1e0bb043cdcf96
Vielka <vvvillav@espol.edu.ec>
2020-07-12 01:28:23 -0500

    Pagina Informacion

---
+++ src/pages/information.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import SEO from '../components/seo'
+import Layout from '@components/Layout'
+import FormPage from './information'
+
+const Information = (props) => {
+  var LOCATION_KEY = 'AIzaSyAp8PdAo33pwVlNZKxlmbcHUOBeTfghXW4'
+  return (
+    <Layout title="Información">
+      <SEO title="Información" />
+
+    </Layout>
+  )
+}
+
+export default Information
+
