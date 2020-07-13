81809626cfa828373f3412c7b6ff9a9ea2552a73
Vielka <vvvillav@espol.edu.ec>
2020-07-12 23:41:31 -0500

    Agregar contenido en la pagina INFORMACION

--- src/pages/information.js
+++ src/pages/information.js
@@ -1,23 +1,25 @@
 import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import SEO from '../components/seo'
-import AlertIcon from '@Icons/AlertIcon'
 import Layout from '@components/Layout'
 
 const Information = (props) => (
     <Layout title="Información">
         <SEO title="Información" />
         <div className="info">
+            <p class="sm:col-span-3">
+                Usa un correo fijo donde puedas ser contactado
+            </p>
             <div className="flex justify-center">
-        <Link to="/">
-          <div className="p-4 bg-blue-700 hover:bg-blue-500 text-white hover:text-gray-700 text-center rounded-lg">
-            Go back to the homepage
-          </div>
-        </Link>
-      </div>
-    </div>
-  </Layout>
-)
+                <Link to="/">
+                <div className="p-4 bg-blue-700 hover:bg-blue-500 text-white hover:text-gray-700 text-center rounded-lg">
+                    Go back to the homepage
+                </div>
+                </Link>
+            </div>
+        </div>
+    </Layout>
+    )
 
 export default Information
 
