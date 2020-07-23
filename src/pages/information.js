dc65c1ea3029ac22736af5030341e15f83c888c4
fponce06 <fponce@espol.edu.ec>
2020-07-22 23:41:58 -0500

    add information section

--- src/pages/information.js
+++ src/pages/information.js
@@ -17,27 +17,36 @@ const Information = (props) => (
         </div>
       </div>
       <div class="rounded-b-lg border-2 border-opacity-25 border-gray-600 p-3 mb-6">
+        <div class="px-5 mb-5 w-100">
+          <h2 class="font-medium text-xl">¿Qué hacemos?</h2>
+          <p class="px-5 pt-2 text-justify">
+            Somos un grupo de estudiantes de la Escuela Superior Politecnica del
+            Litoral (ESPOL) de sexto semestre de la carrera de Ingenieria en
+            Ciencias Computacionales que están viendo la materia de Desarrollo
+            de Aplicaciones Web y Móviles (DAWM), enfocados en realizar un
+            proyecto que permita a los estudiantes adquirir conocimientos
+            brindandoles material de apoyo.
+          </p>
+        </div>
         <div class="px-5 grid grid-flow-col grid-cols-1 xl:grid-cols-2 xl:grid-rows-1 grid-rows-2 gap-4 p-6">
           <div class="mb-5 w-100">
             <h2 class="font-medium text-xl">Misión</h2>
             <p class="px-5 pt-2 text-justify">
-              Resolver la accesibilidad a foros con información fiable, 
-              ejercicios resueltos o material de estudios gratuitos de 
-              materias, ya que esto ocasiona que los estudiantes que se 
-              encuentran con dudas de ejercicios o temas no puedan
-              aclararlas, lo que afecta su rendimiento académico y genera
-              frustración en ellos.
+              Resolver la accesibilidad a foros con información fiable,
+              ejercicios resueltos o material de estudios gratuitos de materias,
+              ya que esto ocasiona que los estudiantes que se encuentran con
+              dudas de ejercicios o temas no puedan aclararlas, lo que afecta su
+              rendimiento académico y genera frustración en ellos.
             </p>
           </div>
           <div class="w-100">
             <h2 class="font-medium text-xl">Visión</h2>
             <p class="px-5 pt-2 text-justify">
-              Tener un ambiente de consulta para las materias, compartir 
-              respuestas o realizar nuevas preguntas de un ejercicio y 
-              generar discusión entre estudiantes sobre cómo resolver un 
-              ejercicio, de tal forma que los usuarios puedan llenarse de 
-              conocimientos y ayudar en el desarrollo intelectual de jovenes
-              y adultos.
+              Tener un ambiente de consulta para las materias, compartir
+              respuestas o realizar nuevas preguntas de un ejercicio y generar
+              discusión entre estudiantes sobre cómo resolver un ejercicio, de
+              tal forma que los usuarios puedan llenarse de conocimientos y
+              ayudar en el desarrollo intelectual de jovenes y adultos.
             </p>
           </div>
         </div>
@@ -51,36 +60,36 @@ const Information = (props) => (
         </h3>
       </div>
       <div class="rounded-b-lg border-2 border-opacity-25 border-gray-600 p-3 mb-6">
-      <div class="grid grid-flow-col grid-cols-1 xl:grid-cols-4 xl:grid-rows-1 grid-rows-4 gap-4 p-6">
-        <Cards
-          title="Desarrolador Web"
-          description=""
-          img="https://www.marketingdirecto.com/wp-content/uploads/2020/03/dia-de-la-mujer-monica-moro.png"
-          name="Kelly Castro Figueroa"
-          email="keescast@espol.edu.ec"
-        />
-        <Cards
-          title="Desarrolador Web"
-          description=""
-          img="https://www.cdc.gov/preconception/spanish/images/man-with-arms-crossed.jpg"
-          name="Carlos Carvajal Villegas"
-          email="cxcarvaj@espol.edu.ec"
-        />
-        <Cards
-          title="Desarrolador Web"
-          description=""
-          img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_uRqSEPeElYHOyR8_6opHrtKg-n7zpjihUA&usqp=CAU"
-          name="Freddy Ponce Muzzio"
-          email="fponce@espol.edu.ec"
-        />
-        <Cards
-          title="Desarrolador Web"
-          description=""
-          img="https://dam.vanidades.com/wp-content/uploads/2019/04/900katiebouman-770x513.jpg"
-          name="Vielka Villavicencio Vallejo"
-          email="vvvillav@espol.edu.ec"
-        />
-      </div>
+        <div class="grid grid-flow-col grid-cols-1 xl:grid-cols-4 xl:grid-rows-1 grid-rows-4 gap-4 p-6">
+          <Cards
+            title="Desarrolador Web"
+            description=""
+            img="https://www.marketingdirecto.com/wp-content/uploads/2020/03/dia-de-la-mujer-monica-moro.png"
+            name="Kelly Castro Figueroa"
+            email="keescast@espol.edu.ec"
+          />
+          <Cards
+            title="Desarrolador Web"
+            description=""
+            img="https://www.cdc.gov/preconception/spanish/images/man-with-arms-crossed.jpg"
+            name="Carlos Carvajal Villegas"
+            email="cxcarvaj@espol.edu.ec"
+          />
+          <Cards
+            title="Desarrolador Web"
+            description=""
+            img="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS_uRqSEPeElYHOyR8_6opHrtKg-n7zpjihUA&usqp=CAU"
+            name="Freddy Ponce Muzzio"
+            email="fponce@espol.edu.ec"
+          />
+          <Cards
+            title="Desarrolador Web"
+            description=""
+            img="https://dam.vanidades.com/wp-content/uploads/2019/04/900katiebouman-770x513.jpg"
+            name="Vielka Villavicencio Vallejo"
+            email="vvvillav@espol.edu.ec"
+          />
+        </div>
       </div>
     </div>
   </Layout>
