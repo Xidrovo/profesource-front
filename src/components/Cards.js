36445b879577a5ea4c30b42d79f8449e5de817b2
CarlosXavier <cxcarvaj@espol.edu.ec>
2020-07-22 21:07:34 -0500

    Add card component

---
+++ src/components/Cards.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Cards = (props) => {
+  return (
+    <div class="relative overflow-hidden mb-8 w-1/2">
+      <div class="rounded-l rounded-r hover:border-blue-900 border-l border-r border-t border-b border-gray-400 lg:border-l lg:border-t lg:border-gray-400 bg-white lg:rounded-b lg:rounded-l lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div class="mb-8">
+          <div class="text-gray-900 font-bold text-xl mb-2">{props.title}</div>
+          <p class="text-gray-700 text-base">{props.description}</p>
+        </div>
+        <div class="flex items-center">
+          <img
+            class="w-10 h-10 rounded-full mr-4"
+            src={props.img}
+            alt="Avatar of Developer" 
+          />
+          <div class="text-sm">
+            <p class="text-gray-900 leading-none">{props.name}</p>
+            <p class="text-gray-600">{props.email}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Cards
--- src/components/FilterPost.js
+++ src/components/FilterPost.js
@@ -5,7 +5,7 @@ import RecentIcon from '@Icons/RecentIcon'
 function FilterPost() {
   return (
     <div className="md:ml-24 flex items-center space-x-2 border-solid border-2 border-gray-101 w-3/4 bg-gray-101 text-sm h-auto ml-12 my-5 rounded-lg justify-center">
-      <div className="inline-flex py-1 ml-2  justify-center">
+      <div className="inline-flex w-1/3 justify-center">
         <button className="flex items-center bg-white px-4 border-gray-104  rounded-lg hover:bg-transparent focus:outline-none hover:border-transparent">
           <span className="text-blue-105 font-bold text-xs md:text-base">
             Todo
